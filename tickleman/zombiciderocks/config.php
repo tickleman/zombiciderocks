5a013fb379bb170ac937efad46cb00ca1efc57a6
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-03-15 00:12:34 +0100

    zombicide skin : start styling the app

--- tickleman/zombiciderocks/config.php
+++ tickleman/zombiciderocks/config.php
@@ -2,6 +2,11 @@
 namespace Tickleman\ZombicideRocks;
 
 use ITRocks\Framework\Configuration;
+use ITRocks\Framework\Locale;
+use ITRocks\Framework\Locale\Language;
+use ITRocks\Framework\Locale\Number_Format;
+use ITRocks\Framework\Plugin\Priority;
+use ITRocks\Framework\View;
 
 global $loc;
 require __DIR__ . '/../../loc.php';
@@ -11,4 +16,21 @@ $config['Tickleman/ZombicideRocks'] = [
 	Configuration::APP         => Application::class,
 	Configuration::ENVIRONMENT => $loc[Configuration::ENVIRONMENT],
 	Configuration::EXTENDS_APP => 'ITRocks/Framework',
+
+	//------------------------------------------------------------------------------ Priority::NORMAL
+	Priority::NORMAL => [
+		Locale::class => [
+			Locale::DATE     => 'd/m/Y',
+			Locale::LANGUAGE => Language::FR,
+			Locale::NUMBER   => [
+				Number_Format::DECIMAL_MINIMAL_COUNT => 2,
+				Number_Format::DECIMAL_MAXIMAL_COUNT => 4,
+				Number_Format::DECIMAL_SEPARATOR     => ',',
+				Number_Format::THOUSAND_SEPARATOR    => SP
+			]
+		],
+		View::class => [
+			View\Html\Engine::CSS => 'zombicide'
+		]
+	]
 ];
