450921b2d86881b7b99c9cc9e2d312f73e9bd038
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-07-09 14:34:57 +0200

    FIX Windows URI and title update

---
+++ tickleman/zombiciderocks/js/application.js
@@ -0,0 +1,43 @@
+$('document').ready(function()
+{
+    window.zindex_counter = 0;
+
+    $('body').build(function()
+    {
+        if (!this.length) return;
+
+        this.xtarget({
+            auto_empty:      {'#main': '#messages'},
+            draggable_blank: '.window>h2',
+            popup_element:   'section',
+            url_append:      'as_widget',
+            success:         function() { $(this).autofocus(); },
+            history: {
+                condition: 'h2:first-of-type',
+                title:     'h2:first-of-type'
+            }
+        });
+
+        // messages is draggable and closable
+        this.inside('#messages').draggable().click(function(event)
+        {
+            if (event.offsetX > this.clientWidth - 10 && event.offsetY < 10) {
+                $(this).empty();
+            }
+        });
+
+        // tab controls
+        this.inside('.tabber').tabber();
+
+        // draggable objects brought to front on mousedown
+        this.inside('.ui-draggable').mousedown(function()
+        {
+            $(this).css('z-index', ++window.zindex_counter);
+        });
+
+        // change all titles attributes to tooltips
+        this.tooltip();
+
+    }).autofocus();
+
+});
