ee20c2972ceb08f1c4c58ba06378aef29f067bd4
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-08-18 11:12:01 +0200

    STD application.js tab indents

--- tickleman/zombiciderocks/js/application.js
+++ tickleman/zombiciderocks/js/application.js
@@ -1,43 +1,40 @@
 $('document').ready(function()
 {
-    window.zindex_counter = 0;
+	window.zindex_counter = 0;
 
-    $('body').build(function()
-    {
-        if (!this.length) return;
+	$('body').build(function()
+	{
+		if (!this.length) return;
 
-        this.xtarget({
-            auto_empty:      {'#main': '#messages'},
-            draggable_blank: '.window>h2',
-            popup_element:   'section',
-            url_append:      'as_widget',
-            success:         function() { $(this).autofocus(); },
-            history: {
-                condition: 'h2:first-of-type',
-                title:     'h2:first-of-type'
-            }
-        });
+		this.xtarget({
+			auto_empty:      {'#main': '#messages'},
+			draggable_blank: '.window>h2',
+			history:         { condition: 'h2:first-of-type', title: 'h2:first-of-type' },
+			popup_element:   'section',
+			success:         function() { $(this).autofocus(); },
+			url_append:      'as_widget'
+		});
 
-        // messages is draggable and closable
-        this.inside('#messages').draggable().click(function(event)
-        {
-            if (event.offsetX > this.clientWidth - 10 && event.offsetY < 10) {
-                $(this).empty();
-            }
-        });
+		// messages is draggable and closable
+		this.inside('#messages').draggable().click(function(event)
+		{
+			if ((event.offsetX > (this.clientWidth - 10)) && (event.offsetY < 10)) {
+				$(this).empty();
+			}
+		});
 
-        // tab controls
-        this.inside('.tabber').tabber();
+		// tab controls
+		this.inside('.tabber').tabber();
 
-        // draggable objects brought to front on mousedown
-        this.inside('.ui-draggable').mousedown(function()
-        {
-            $(this).css('z-index', ++window.zindex_counter);
-        });
+		// draggable objects brought to front on mousedown
+		this.inside('.ui-draggable').mousedown(function()
+		{
+			$(this).css('z-index', ++window.zindex_counter);
+		});
 
-        // change all titles attributes to tooltips
-        this.tooltip();
+		// change all titles attributes to tooltips
+		this.tooltip();
 
-    }).autofocus();
+	}).autofocus();
 
 });
